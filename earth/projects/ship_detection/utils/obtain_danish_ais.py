6ff5d3c2e9ab544921c4436c445507f9d70d56eb
Frank Wang <frankwang@frankwangdesign.com>
2017-11-05 10:28:27 -0800

    Fixed indexing issue

--- earth/projects/ship_detection/utils/obtain_danish_ais.py
+++ earth/projects/ship_detection/utils/obtain_danish_ais.py
@@ -2,8 +2,9 @@ import time
 
 from io import StringIO
 from sqlalchemy import create_engine
-import urllib.request as urllib
+import subprocess as sp
 from multiprocessing import Process
+import os
 
 import pandas as pd
 import numpy as np
@@ -11,10 +12,12 @@ import numpy as np
 
 def get_process_image(i):
     print('working on ftp://ftp.ais.dk/ais_data/aisdk_{}.csv...'.format(i))
-    req = urllib.Request('ftp://ftp.ais.dk/ais_data/aisdk_{}.csv'.format(i))
-    data = str(urllib.urlopen(req).read())
 
-    df = pd.read_csv(StringIO(data))
+    print(sp.call('wget ftp://ftp.ais.dk/ais_data/aisdk_{}.csv'.format(i), shell=True, stderr=open(os.devnull, 'wb')))
+    with open('aisdk_{}.csv'.format(i), 'r') as f:
+        df = pd.read_csv(f)
+    print('downloaded aisdk_{}.csv'.format(i))
+
     del df['Cargo type']
     del df['Destination']
     del df['ETA']
@@ -45,10 +48,15 @@ def get_process_image(i):
     df.replace('Unknown', np.NaN, inplace=True)
     df.replace('Undefined', np.NaN, inplace=True)
     df.replace('Unknown value', np.NaN, inplace=True)
+    df.replace('Unk', np.NaN, inplace=True)
+    print('processed aisdk_{}.csv'.format(i))
 
-    conn = create_engine("mysql://{}@{}/{}".format('root', '104.199.118.158', 'earthdat'))
-    for i in range(int(df.shape[0] / 500000) + 1):
-        df[500000*i:500000*(i+1)].to_sql('denmark_ais', conn, if_exists='append', index=False)
+    conn = create_engine("mysql://{}@{}/{}".format(
+        'root', '104.199.118.158', 'earthdat'
+    ))
+    for j in range(int(df.shape[0] / 500000) + 1)[:5]:
+        df[500000*j:500000*(j+1)].to_sql('denmark_ais', conn, if_exists='append', index=False)
+        print('uploaded iteration {}/{} for aisdk_{}.csv'.format(j + 1, int(df.shape[0] / 500000) + 1, i))
 
 processes = []
 for i in range(20171001, 20171030):
