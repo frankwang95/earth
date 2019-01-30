53155b9fb7f6fa1ebdef0d094d0cd66f0ed35c6d
Frank Wang <frankwang@frankwangdesign.com>
2019-01-30 12:27:24 -0600

    Fixed server endpoints.

--- collection_landsat/server.py
+++ collection_landsat/server.py
@@ -36,39 +36,17 @@ scheduler = Scheduler(
 server = get_flask_server()
 
 
-@server.route('/push/<lid>', methods=["POST"])
-def push(lid):
-    scheduler.push(lid)
-    return "{}"
+@server.route('/push', methods=["POST"])
+def push():
+    row = request.args.get('row', type=int, default=None)
+    path = request.args.get('path', type=int, default=None)
+    assert type(row) == type(path)
+    return json.dumps(scheduler.push_next_block(row=row, path=path))
 
 
-@server.route('/process_queue', methods=["GET"])
-def get_process_queue():
-    p_queue = scheduler.get_p_queue()
-    response = {
-        "p_queue_len": len(p_queue),
-        "p_queue": p_queue
-    }
-    return json.dumps(response)
-
-
-@server.route('/download_queue', methods=["GET"])
-def get_download_queue():
-    d_queue = scheduler.get_d_queue()
-    response = {
-        "d_queue_len": len(d_queue),
-        "d_queue": d_queue
-    }
-    return json.dumps(response)
-
-
-@server.route('/check_for_scene/<lid>', methods=["GET"])
-def check_for_scene(lid):
-    response = {
-        "scene_id": lid,
-        "is_present": scheduler.check_scene_exists(lid)
-    }
-    return json.dumps(response)
+@server.route('/get_queue')
+def get_queue():
+    return json.dumps(scheduler.pending_work)
 
 
 if __name__ == "__main__":
